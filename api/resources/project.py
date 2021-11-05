4029ec090d5cab124544fcd594b048814713e5e9
James Zhou <jameszhou@g.ucla.edu>
2021-11-05 13:42:15 -0700

    connected to heroku db

--- api/resources/project.py
+++ api/resources/project.py
@@ -3,8 +3,7 @@ import psycopg2
 from flask_restful import Resource
 
 DATABASE_URL = os.environ['DATABASE_URL']
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-conn = psycopg2.connect(DATABASE_URL)
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 
 
