56f97ec8b087460bbbcd3959e209720da5951a8e
James Zhou <jameszhou@g.ucla.edu>
2021-11-05 11:25:13 -0700

    initial heroku setup

---
+++ api/resources/project.py
@@ -0,0 +1,14 @@
+import os
+import psycopg2
+from flask_restful import Resource
+
+DATABASE_URL = os.environ['DATABASE_URL']
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(DATABASE_URL)
+cur = conn.cursor()
+
+
+class Project(Resource):
+    def get(self):
+        cur.execute("SELECT * from projects")
+        return cur.fetchall()
