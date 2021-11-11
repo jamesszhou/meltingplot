d75b561e63f6477c8e4482dcc3c055443f1b5e32
Thomas Kaneshige <tykaneshige.work@gmail.com>
2021-11-11 00:59:41 -0800

    Rough implementation of the graph component.

---
+++ meltingplot-frontend/src/components/Content/GraphComponent.js
@@ -0,0 +1,89 @@
+import React from "react";
+
+import loadingCircle from "../../assets/img/LoadingCircle.gif";
+
+class GraphComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            
+            url: process.env.DATABASE_URL,
+            config: props.config,
+
+            error: null,
+            graph: null
+        };
+
+        // Bind function to the class
+        this.generateGraph = this.generateGraph.bind(this);
+    }
+
+    generateGraph() {
+        // Make fetch request
+        fetch(this.url, {
+            body: this.config,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(
+            (response) => {
+                let jsonResponse = JSON.parse(response.json);
+                this.setState({
+                    fetched: true,
+                    isLoaded: true,
+                    graph: jsonResponse.img
+                })
+            },
+            (error) => {
+                this.setState({
+                    fetched: true,
+                    isLoaded: true,
+                    error
+                })
+            }
+        );   
+    }
+
+    render() {
+        const { isLoaded, error, graph } = this.state;
+
+        // Verify graph fetch has not encountered an error
+        if (error) {
+            return (
+                <div>
+                    Error: {error.message}
+                </div>
+            );
+        } else {
+            // Verify that graph fetch has been called at least once
+            if (!graph) {
+                return (
+                    <div>
+                        Click "Generate" to display your graph!
+                        <button onClick={this.generateGraph}>Generate</button>
+                    </div>
+                );
+            } else {
+                // Verify that graph fetch has completed
+                if (!isLoaded) {
+                    return (
+                        <div>
+                            <img src={loadingCircle}></img>
+                        </div>
+                    );
+                } else {
+                    return (
+                        <div>
+                            <img src={graph}></img>
+                            <button onClick={this.generateGraph}>Generate</button>
+                        </div>
+                    );
+                }
+            }
+        }
+    }
+}
+
+export default GraphComponent;
--- meltingplot-frontend/src/views/LandingPage.js
+++ meltingplot-frontend/src/views/LandingPage.js
@@ -3,11 +3,13 @@ import React from "react";
 // core components
 import ProjectSpecs from "../components/Content/ProjectSpecs.js";
 import PageFooter from "../components/Footers/PageFooter.js";
+import GraphComponent from "../components/Content/GraphComponent.js";
 
 function LandingPage() {
   return (
     <>
       <ProjectSpecs/>
+      <GraphComponent/>
       <PageFooter/>
     </>
   );
