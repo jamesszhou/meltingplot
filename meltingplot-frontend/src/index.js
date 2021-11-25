2b9becba0d21e694a6009c4f442c0c1c1af1c3a1
Bradley <pickbradley23@gmail.com>
2021-11-24 18:44:09 -0800

    Create page to list all projects of a given user

--- meltingplot-frontend/src/index.js
+++ meltingplot-frontend/src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from "react-dom";
 import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // pages
-
 import LandingPage from "./views/LandingPage.js";
+import ProjectsPage from "./views/ProjectsPage.js";
+
 // others
 
 ReactDOM.render(
   <HashRouter>
     <Switch>
-      <Route path="/index" render={(props) => <LandingPage {...props} />} />
-      <Redirect to="/index" />
+      <Route path="/index">
+        <LandingPage />
+      </Route>
+      <Route path="/project">
+        <ProjectsPage />
+      </Route>
     </Switch>
   </HashRouter>,
   document.getElementById("root")
