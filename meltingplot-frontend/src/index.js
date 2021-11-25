2c036e04ad2445da814c811676d3f45e4f7abba0
Thomas Kaneshige <tykaneshige.work@gmail.com>
2021-11-24 19:17:37 -0800

    Added functionality for the front page. Needs to be formatted.

--- meltingplot-frontend/src/index.js
+++ meltingplot-frontend/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // pages
-
+import FrontPage from "./views/FrontPage.js";
 import LandingPage from "./views/LandingPage.js";
 // others
 
 ReactDOM.render(
   <HashRouter>
     <Switch>
-      <Route path="/index" render={(props) => <LandingPage {...props} />} />
+      <Route path="/index" render={(props) => <FrontPage {...props} />} />
     </Switch>
   </HashRouter>,
   document.getElementById("root")
---
+++ meltingplot-frontend/src/views/FrontPage.js
@@ -0,0 +1,71 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Login from '../components/Content/Login.js';
+import CreateAccount from "../components/Content/CreateAccount.js";
+
+import {
+    Button
+} from "reactstrap";
+
+import background from '../assets/img/Samueli.jpeg';
+
+function FrontPage() {
+
+    const [loginModal, setLoginModal] = React.useState(false);
+    const [usernameModal, setUsernameModal] = React.useState(false);
+    const [passwordModal, setPasswordModal] = React.useState(false);
+    const [checkModal, setCheckModal] = React.useState(false);
+
+    const toggleLoginModal = () => {
+        setLoginModal(!loginModal);
+    };
+
+    const toggleUsernameModal = () => {
+        setUsernameModal(!usernameModal);
+    };
+
+    const togglePasswordModal = () => {
+        setPasswordModal(!passwordModal);
+    };
+
+    const toggleCheckModal = () => {
+        setCheckModal(!checkModal);
+    }
+
+    return (
+        <div style={{ 
+            backgroundImage: `url(${background})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            width: '100vw',
+            height: '100vh'
+        }}>
+            <Button  
+                onClick={toggleLoginModal}
+            >
+                Login
+            </Button>
+            <Button
+                onClick={toggleUsernameModal}
+            >
+                Create Account
+            </Button>
+            <Login
+                loginModal={loginModal}
+                toggleLoginModal={toggleLoginModal}
+            />
+            <CreateAccount
+                usernameModal={usernameModal}
+                passwordModal={passwordModal}
+                checkModal={checkModal}
+                toggleUsernameModal={toggleUsernameModal}
+                togglePasswordModal={togglePasswordModal}
+                toggleCheckModal={toggleCheckModal}
+            />
+        </div>
+    );
+}
+
+export default FrontPage;
